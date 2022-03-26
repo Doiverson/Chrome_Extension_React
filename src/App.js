cc8bff46aaa46ba06ad32e39bb9c8f3930a59d37
Shosuke Doi <sho@Shosukes-MacBook-Pro.local>
2022-03-26 12:08:12 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+/*global chrome*/
+import React, { useEffect, useState } from "react";
+/// <reference path="pathTo/chrome.d.ts"/>
+import { ChromeMessage, Sender } from "./types";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [url, setUrl] = useState('');
+    const [responseFromContent, setResponseFromContent] = useState('');
+
+    useEffect(() => {
+        const queryInfo = {active: true, lastFocusedWindow: true};
+
+        chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+            const url = tabs[0].url;
+            setUrl(url);
+        });
+    }, []);
+
+    const sendTestMessage = () => {
+        const message: ChromeMessage = {
+            from: Sender.React,
+            message: "Hello from React",
+        }
+
+        const queryInfo: chrome.tabs.QueryInfo = {
+            active: true,
+            currentWindow: true
+        };
+
+        /**
+         * We can't use "chrome.runtime.sendMessage" for sending messages from React.
+         * For sending messages from React we need to specify which tab to send it to.
+         */
+        chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+            const currentTabId = tabs[0].id;
+            /**
+             * Sends a single message to the content script(s) in the specified tab,
+             * with an optional callback to run when a response is sent back.
+             *
+             * The runtime.onMessage event is fired in each content script running
+             * in the specified tab for the current extension.
+             */
+            chrome.tabs.sendMessage(
+                currentTabId,
+                message,
+                (response) => {
+                    setResponseFromContent(response);
+                });
+        });
+    };
+
+
+    return (
+        <div className="App">
+            <h1>Olive</h1>
+            <p>{url}</p>
+            <button onClick={sendTestMessage}>SEND MESSAGE</button>
+        </div>
+    );
 }
 
 export default App;
