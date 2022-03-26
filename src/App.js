beee253edc517b4e406fa0860040ab9319a3cc31
Shosuke Doi <sho@Shosukes-MacBook-Pro.local>
2022-03-26 12:08:24 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,62 +1,72 @@
 /*global chrome*/
 import React, { useEffect, useState } from "react";
-/// <reference path="pathTo/chrome.d.ts"/>
-import { ChromeMessage, Sender } from "./types";
+import data from "./data/members.json";
+
+import logo from "./favicon.png";
+import logoWhite from "./white_logo.png"
+
 import './App.css';
+import { Checkbox, Layout, Button } from 'antd';
+const { Header, Content } = Layout;
 
 function App() {
 
     const [url, setUrl] = useState('');
-    const [responseFromContent, setResponseFromContent] = useState('');
+    const [ test, setTest ] = useState([]);
 
     useEffect(() => {
-        const queryInfo = {active: true, lastFocusedWindow: true};
 
-        chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
-            const url = tabs[0].url;
-            setUrl(url);
-        });
+        chrome.storage.sync.get('checks', (results) => {
+            if (results.checks) {
+                setTest(results.checks)
+            }
+        })
+
+        // const queryInfo = {active: true, lastFocusedWindow: true};
+        // chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+        //     const url = tabs[0].url;
+        //     setUrl(url);
+        // });
     }, []);
 
-    const sendTestMessage = () => {
-        const message: ChromeMessage = {
-            from: Sender.React,
-            message: "Hello from React",
+    const onChange = (e) => {
+        if (test.indexOf(e.target.value) === -1) {
+            setTest([...test, e.target.value]);
+            chrome.storage.sync.set({ checks: [...test, e.target.value]});
+        } else {
+            setTest(test.filter(x => x !== e.target.value));
+            chrome.storage.sync.set({ checks: test.filter(x => x !== e.target.value)});
         }
+    }
 
-        const queryInfo: chrome.tabs.QueryInfo = {
-            active: true,
-            currentWindow: true
-        };
-
-        /**
-         * We can't use "chrome.runtime.sendMessage" for sending messages from React.
-         * For sending messages from React we need to specify which tab to send it to.
-         */
-        chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
-            const currentTabId = tabs[0].id;
-            /**
-             * Sends a single message to the content script(s) in the specified tab,
-             * with an optional callback to run when a response is sent back.
-             *
-             * The runtime.onMessage event is fired in each content script running
-             * in the specified tab for the current extension.
-             */
-            chrome.tabs.sendMessage(
-                currentTabId,
-                message,
-                (response) => {
-                    setResponseFromContent(response);
-                });
-        });
+    const onHandleClearAll = () => {
+        setTest([]);
+        chrome.storage.sync.clear();
     };
 
-
     return (
-        <div className="App">
-            <h1>Olive</h1>
-            <p>{url}</p>
-            <button onClick={sendTestMessage}>SEND MESSAGE</button>
+        <div className="olive">
+            <Layout>
+                <div style={{position: 'sticky', top: 0, zIndex: 100}}>
+                    <Header style={{background: "white", textAlign: "center"}}>
+                        <div style={{width: 40, height: 40, margin: '0 auto'}}>
+                            <img src={logoWhite} style={{maxWidth: '100%'}}/>
+                        </div>
+                    </Header>
+                    <div style={{textAlign: 'right', margin: 5}}>
+                        <Button style={{borderRadius: 5, width: '100%'}} onClick={onHandleClearAll}>Clear</Button>
+                    </div>
+                </div>
+                <Content>
+                    <div style={{display: "flex", flexDirection: "column", marginLeft: 20}}>
+                        {
+                            data.map(x => <Checkbox value={x.name} checked={test?.includes(x.name)} onChange={onChange} style={{marginLeft: 0}}>
+                                <div className="member-name">{x.name}</div>
+                            </Checkbox>)
+                        }
+                    </div>
+                </Content>
+            </Layout>
         </div>
     );
 }
